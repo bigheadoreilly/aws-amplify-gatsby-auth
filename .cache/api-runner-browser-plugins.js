d5b0e74ef74c22e259f5c44ef449ce672e2fecaf
Michael O'Reilly <mjj.oreilly@gmail.com>
2021-05-09 21:00:02 +0100

    Adding some tweaks to color

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"raff-fitness","short_name":"fitness","start_url":"/","background_color":"#295c07","theme_color":"#295c07","display":"minimal-ui","icon":"src/images/gatsby-icon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"4a9773549091c227cd2eb82ccd9c5e3a"},
+    },{
+      plugin: require('../gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
