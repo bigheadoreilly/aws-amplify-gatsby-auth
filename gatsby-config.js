8788c62c28a5b6f545c5e7f65b6fbe33dd2ec960
Michael O'Reilly <mjj.oreilly@gmail.com>
2021-05-09 19:43:16 +0100

    Updating the title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Raff Fitness Starter`,
+    description: `Placeholder for the incoming fitness planning applications.`,
     author: `@gatsbyjs`,
   },
   plugins: [
